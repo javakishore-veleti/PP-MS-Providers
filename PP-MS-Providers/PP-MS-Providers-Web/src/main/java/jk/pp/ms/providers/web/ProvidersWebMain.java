45361e60ca4efea29c9fb7982045549bd6b8a63c
KISHORE VELETI <vavkkishore@kishores-mbp.home>
2021-05-22 09:35:40 -0400

    Initial Commit

---
+++ PP-MS-Providers/PP-MS-Providers-Web/src/main/java/jk/pp/ms/providers/web/ProvidersWebMain.java
@@ -0,0 +1,22 @@
+package jk.pp.ms.providers.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import jk.pp.ms.commons.configs.MSCommonGlobalConfig;
+import jk.pp.ms.commons.configs.MSType;
+
+@SpringBootApplication
+@Import(MSCommonGlobalConfig.class)
+public class ProvidersWebMain {
+
+	public static void main(String[] args) {
+
+		SpringApplication app = new SpringApplication(ProvidersWebMain.class);
+		app.setAdditionalProfiles(MSCommonGlobalConfig.buildAMSStartupProfiles(MSType.PROVIDERS));
+
+		app.run(args);
+	}
+
+}
