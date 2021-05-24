d7f620d3b47856c47982f2c15a265b99754a5a9c
KISHORE VELETI <vavkkishore@kishores-mbp.home>
2021-05-24 15:55:31 -0400

    Apache Kafka initial commit

--- PP-MS-Providers/PP-MS-Providers-Web/src/main/java/jk/pp/ms/providers/web/controller/ProviderInfoCrudController.java
+++ PP-MS-Providers/PP-MS-Providers-Web/src/main/java/jk/pp/ms/providers/web/controller/ProviderInfoCrudController.java
@@ -1,5 +1,9 @@
 package jk.pp.ms.providers.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +20,34 @@ public class ProviderInfoCrudController extends AppCrudController<ProviderInfo, 
 	@Autowired
 	public ProviderInfoCrudController(ProviderInfoCrudService service) {
 		super.setCrudService(service);
+	}
+
+	@Override
+	public List<ProviderInfoCrudDTO> populateInitialDataDomais() {
+
+		List<ProviderInfoCrudDTO> initialDataDomains = new ArrayList<ProviderInfoCrudDTO>();
+
+		ProviderInfoCrudDTO crudDTO = null;
+		ProviderInfo asset = null;
+
+		int length = 25;
+		boolean useLetters = true;
+		boolean useNumbers = false;
+		String generatedString = null;
+
+		for (int ctr = 1; ctr <= 10; ++ctr) {
+			crudDTO = new ProviderInfoCrudDTO();
+
+			asset = new ProviderInfo();
+
+			generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+
+			asset.setName(generatedString);
+			crudDTO.setDomain(asset);
+
+			initialDataDomains.add(crudDTO);
+		}
+
+		return initialDataDomains;
 	}
 }
--- PP-MS-Providers/PP-MS-Providers-Web/src/main/java/jk/pp/ms/providers/web/controller/ProviderTyperudController.java
+++ PP-MS-Providers/PP-MS-Providers-Web/src/main/java/jk/pp/ms/providers/web/controller/ProviderTyperudController.java
@@ -1,5 +1,9 @@
 package jk.pp.ms.providers.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +20,34 @@ public class ProviderTyperudController extends AppCrudController<ProviderType, P
 	@Autowired
 	public ProviderTyperudController(ProviderTypeCrudService service) {
 		super.setCrudService(service);
+	}
+
+	@Override
+	public List<ProviderTypeCrudDTO> populateInitialDataDomais() {
+
+		List<ProviderTypeCrudDTO> initialDataDomains = new ArrayList<ProviderTypeCrudDTO>();
+
+		ProviderTypeCrudDTO crudDTO = null;
+		ProviderType asset = null;
+
+		int length = 25;
+		boolean useLetters = true;
+		boolean useNumbers = false;
+		String generatedString = null;
+
+		for (int ctr = 1; ctr <= 10; ++ctr) {
+			crudDTO = new ProviderTypeCrudDTO();
+
+			asset = new ProviderType();
+
+			generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+
+			asset.setName(generatedString);
+			crudDTO.setDomain(asset);
+
+			initialDataDomains.add(crudDTO);
+		}
+
+		return initialDataDomains;
 	}
 }
